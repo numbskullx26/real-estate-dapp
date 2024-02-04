7997a369e011c9f9e9312cf7ef78e83283d998f3
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2024-02-04 12:35:52 +0530

    add : Deployement of Escrow.sol

--- test/Escrow.js
+++ test/Escrow.js
@@ -7,9 +7,55 @@ const tokens = (n) => {
 
 describe("Escrow", () => {
   it("saves the addresses", async () => {
-    const RealEstate = await ethers.getContractFactory("RealEstate");
-    const realEstate = await RealEstate.deploy();
+    let buyer, seller, inspector, lender;
+    let escrow, realEstate;
 
-    console.log(realEstate.address);
+    beforeEach(async () => {
+      //setup accounts
+      [buyer, seller, inspector, lender] = await ethers.getSigners(); //getSgners() gives us random accounts with ETH in the blockchai for testing
+
+      //deploy real estate
+      const RealEstate = await ethers.getContractFactory("RealEstate");
+      realEstate = await RealEstate.deploy();
+
+      //Mint
+      let transaction = await realEstate
+        .connect(seller)
+        .mint(
+          "https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS"
+        );
+      await transaction.wait();
+
+      //deploy Escrow
+      const Escrow = await ethers.getContractFactory("Escrow");
+      escrow = await Escrow.deploy(
+        realEstate.address,
+        seller.address,
+        inspector.address, 
+        lender.address
+      ); //passing the various account addresses which were destructured above into the Escrow contract's contructor
+    });
+
+    describe("Deployement", () => {
+      it("Returns NFT address", async () => {
+        const result = await escrow.nftAddress();
+        expect(result).to.be.equal(realEstate.address);
+      });
+
+      it("Returns seller", async () => {
+        const result = await escrow.seller();
+        expect(result).to.be.equal(seller.address);
+      });
+
+      it("Returns inspector", async () => {
+        const result = await escrow.inspector();
+        expect(result).to.be.equal(inspector.address);
+      });
+
+      it("Returns lender", async () => {
+        const result = await escrow.lender();
+        expect(result).to.be.equal(lender.address);
+      });
+    });
   });
 });
