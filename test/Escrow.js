1bde88684340ec228b52190e4a8a9a26fc137e60
Indrajeet Sarkar <sarkarraj1978@gmail.com>
2024-02-04 18:54:09 +0530

    add :  Listing Functionality with tests

--- test/Escrow.js
+++ test/Escrow.js
@@ -31,9 +31,20 @@ describe("Escrow", () => {
       escrow = await Escrow.deploy(
         realEstate.address,
         seller.address,
-        inspector.address, 
+        inspector.address,
         lender.address
       ); //passing the various account addresses which were destructured above into the Escrow contract's contructor
+
+     
+      //Approve property
+      transaction = await realEstate.connect(seller).approve(escrow.address, 1);
+      await transaction.wait();
+
+      //listing
+      transaction = await escrow
+        .connect(seller)
+        .list(1, buyer.address, tokens(10), tokens(5));
+      await transaction.wait();
     });
 
     describe("Deployement", () => {
@@ -55,6 +66,32 @@ describe("Escrow", () => {
       it("Returns lender", async () => {
         const result = await escrow.lender();
         expect(result).to.be.equal(lender.address);
+      });
+    });
+
+    describe("Listing", () => {
+      it("Updates ownership", async () => {
+        expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address);
+      });
+
+      it("Update as Listed", async () => {
+        const result = await escrow.isListed(1);
+        expect(result).to.be.equal(true);
+      });
+
+      it("Returns Buyer", async () => {
+        const result = await escrow.buyer(1);
+        expect(result).to.be.equal(buyer.address);
+      });
+
+      it("Returns pruchase price", async () => {
+        const result = await escrow.purchasePrice(1);
+        expect(result).to.be.equal(tokens(10));
+      });
+
+      it("Returns escrow amount", async () => {
+        const result = await escrow.escrowAmount(1);
+        expect(result).to.be.equal(tokens(5));
       });
     });
   });
